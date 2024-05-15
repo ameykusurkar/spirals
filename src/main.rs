bc724f7986e7cc325d0998bbc26ad984759eb8b7
Amey Kusurkar <amey1000@gmail.com>
2024-05-15 21:13:53 +0100

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use image;
 use image::{DynamicImage, GenericImage, GenericImageView, Rgba};
-use image::imageops::FilterType;
 
 struct Buffer {
     buffer: DynamicImage,
@@ -17,14 +16,15 @@ impl Buffer {
         for i in 0..size {
             for j in 0..size {
                 if self.buffer.in_bounds(x + i, y + j) {
-                    self.buffer.put_pixel(x + i , y + j, Rgba([85, 239, 196, 255]));
+                    self.buffer
+                        .put_pixel(x + i, y + j, Rgba([85, 239, 196, 255]));
                 }
             }
         }
     }
 
     fn save(&self, filename: &str) {
-        self.buffer.resize(1000, 1000, FilterType::Nearest).save(filename).expect("Could not save image");
+        self.buffer.save(filename).expect("Could not save image");
     }
 }
 
@@ -38,24 +38,30 @@ impl PointGenerator {
     fn next_point(&mut self) -> (f32, f32, u32) {
         self.radius += 1.0;
         self.theta += 1.0;
-        (self.radius * self.theta.cos(), self.radius * self.theta.sin(), self.radius as u32)
+        (
+            self.radius * self.theta.cos(),
+            self.radius * self.theta.sin(),
+            self.radius as u32,
+        )
     }
 }
 
 fn main() {
-    let num_points = 100_000;
-    let mut buffer = Buffer::new(num_points);
+    let num_points = 10_000_000;
+    let mut buffer = Buffer::new(2000);
     let mut generator = PointGenerator::default();
 
     for _ in 0..num_points {
         let (x, y, n) = generator.next_point();
+        let scale = 2000.0 / num_points as f32;
         if is_prime(n) {
-            let (buffer_x, buffer_y) = ((x + num_points as f32) as u32, (y + num_points as f32) as u32);
-            buffer.set(buffer_x, buffer_y, 200);
+            let buffer_x = (x + num_points as f32) * scale;
+            let buffer_y = (y + num_points as f32) * scale;
+            buffer.set(buffer_x as u32, buffer_y as u32, 1);
         }
     }
 
-    buffer.save("output.png");
+    buffer.save("test.png");
 }
 
 fn is_prime(n: u32) -> bool {
